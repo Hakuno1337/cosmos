729a4aeb6b69fa14d50c5ea21b9b09a45e869f7a
Hakuno <trikolor702@gmail.com>
2024-12-07 11:51:24 +0700

    Добавил новый коментарий и переписал вывод

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,12 +7,12 @@ int main()
 	// 1-ая задача
 	int a, b, c;
 	double res;
-	cout << "The first is hight, the second is length, the third is width" << endl;
+	cout << "The first is hight, the second is length,3 the third is width" << endl;
 	cin >> a >> b >> c; // Ввод трех целых чисел с клавиатуры 
 	res = sqrt(a * a + b * b + c * c); // Вычисление длины диагонали кирпича
 	cout << fixed;
 	cout.precision(2);
 	// Вывод результата с точностью до dvex carkfksd dfkldfkdfl
-	cout << "\nThe diagonal of the brick with edges " << a << " , "
-		<< b << " and " << c << " is " << res << endl;
+	// Вывод нового результата
+	cout << "\nThe diagonal of the brick with edges " << a << " , " << b << " and " << c << " is " << res << endl;
 }
