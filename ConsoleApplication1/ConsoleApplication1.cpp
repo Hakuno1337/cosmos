dc01bbc19e602225afc8561740498661dd05f37b
Hakuno <trikolor702@gmail.com>
2024-12-07 11:13:17 +0700

    мой первый настоящий коммит

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+
+{
+	// 1-ая задача
+	int a, b, c;
+	double res;
+	cout << "The first is hight, the second is length, the third is width" << endl;
+	cin >> a >> b >> c; // Ввод трех целых чисел с клавиатуры 
+	res = sqrt(a * a + b * b + c * c); // Вычисление длины диагонали кирпича
+	cout << fixed;
+	cout.precision(2);
+	// Вывод результата с точностью до двух знаком после запятой 
+	cout << "\nThe diagonal of the brick with edges " << a << " , "
+		<< b << " and " << c << " is " << res << endl;
+}
